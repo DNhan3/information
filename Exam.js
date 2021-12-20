ecf45eac70f916e3e6dd0d92f08c0aacc31d6fc6
DNhan3 <96424205+DNhan3@users.noreply.github.com>
2021-12-20 19:50:33 +0700

    upload

---
+++ Exam.js
@@ -0,0 +1,7 @@
+function gmail() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("ducnhan141106@gmail.com");
+
+    /* Alert the copied text */
+    alert("You have copied my email address");
+}
