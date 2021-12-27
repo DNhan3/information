eeafd08b2ea0864430e35df289d6d0e32b4f704b
DNhan3 <96424205+DNhan3@users.noreply.github.com>
2021-12-27 23:39:01 +0700

    Change to index name

--- Gangbang.js
+++
@@ -1,7 +0,0 @@
-function gmail() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("ducnhan141106@gmail.com");
-
-    /* Alert the copied text */
-    alert("You have copied my email address");
-}
---
+++ Web.js
@@ -0,0 +1,15 @@
+function phone() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("0943778908");
+
+    /* Alert the copied text */
+    alert("You have copied my phone number");
+}
+
+function email() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("ducnhan141106@gmail.com");
+
+    /* Alert the copied text */
+    alert("You have copied my email address");
+}
