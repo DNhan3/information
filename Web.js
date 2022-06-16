0d145217ae8f38c8b670b667c7eb87e84bab4634
vvkquau <ducnhan141106@gmail.com>
2022-06-17 04:44:46 +0700

    new

--- Web.js
+++ Web.js
@@ -1,6 +1,6 @@
 function phone() {
     /* Copy the text inside the text field */
-    navigator.clipboard.writeText("0937840446");
+    navigator.clipboard.writeText("0815460325");
 
     /* Alert the copied text */
     alert("You have copied my phone number");
@@ -8,8 +8,26 @@ function phone() {
 
 function email() {
     /* Copy the text inside the text field */
-    navigator.clipboard.writeText("ducnhan141106@gmail.com");
+    navigator.clipboard.writeText("anhhuy19012006@gmail.com");
 
     /* Alert the copied text */
     alert("You have copied my email address");
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
 }
