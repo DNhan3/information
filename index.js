a05240f2d47e9555e7d0fef0eb7aafaf352cf0a8
DNhan3 <96424205+DNhan3@users.noreply.github.com>
2023-01-22 17:05:42 +0700

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+function phone() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("0937840446");
+
+    /* Alert the copied text */
+    alert("You have copied my phone  number !");
+}
+
+function email() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("ducnhan141106@gmail.com");
+
+    /* Alert the copied text */
+    alert("You have copied my email address !");
+}
+
+function bank() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("1034561329");
+
+    /* Alert the copied text */
+    alert("You have copied my bank number account ✨");
+}
